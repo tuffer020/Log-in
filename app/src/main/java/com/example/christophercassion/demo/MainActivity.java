442c99b7f88dba3d3782d3088204e7fef94b1884
tuffer020 <tuffer020@gmail.com>
2015-02-09 12:44:56 -0500

    Register button goes to sign_up.

--- app/src/main/java/com/example/christophercassion/demo/MainActivity.java
+++ app/src/main/java/com/example/christophercassion/demo/MainActivity.java
@@ -1,10 +1,48 @@
+//package com.example.christophercassion.demo;
+//
+//import android.support.v7.app.ActionBarActivity;
+//
+//
+//
+//public class MainActivity extends ActionBarActivity {
+//
+//
+//}
 package com.example.christophercassion.demo;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+//import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnClickListener listnr;
+        listnr = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(MainActivity.this, sign_up.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Button btn =(Button) findViewById(R.id.btn);
+        btn.setOnClickListener(listnr);
 
-import android.support.v7.app.ActionBarActivity;
-
-
-
-public class MainActivity extends ActionBarActivity {
-
-
+//        //New thingy
+//        OnClickListener listnr2;
+//        listnr2 = new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent j= new Intent(MainActivity.this, MainActivity.class);
+//                startActivity(j);
+//            }
+//        };
+//        Button btli =(Button) findViewById(R.id.btli);
+//        btli.setOnClickListener(listnr2);
+    }
 }
--- app/src/main/java/com/example/christophercassion/demo/sign_up.java
+++ app/src/main/java/com/example/christophercassion/demo/sign_up.java
@@ -1,10 +1,21 @@
+//package com.example.christophercassion.demo;
+//
+//import android.support.v7.app.ActionBarActivity;
+//
+//
+//
+//public class sign_up extends ActionBarActivity {
+//
+//
+//}
 package com.example.christophercassion.demo;
-
-import android.support.v7.app.ActionBarActivity;
-
-
-
-public class sign_up extends ActionBarActivity {
-
-
+import android.app.Activity;
+import android.os.Bundle;
+public class sign_up extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
 }
